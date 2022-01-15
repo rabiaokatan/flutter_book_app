7419049ab960b350b0b127920da364719d1c1c55
Rabia Okatan <rabiaro26@gmail.com>
2022-01-15 16:22:49 +0300

    add book card and settings page

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_book_app/screens/settings.dart';
+import 'package:flutter_book_app/widgets/book_card.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Anasayfa'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const Settings();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            BookCard(),
+            BookCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/book_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BookCard extends StatelessWidget {
+  const BookCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 15),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.95,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.grey.shade200),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text("Kitap İsmi: "),
+                Text("Yazar: "),
+                Text("Sayfa Sayısı: "),
+                Text("Bitirme Tarihi: "),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
